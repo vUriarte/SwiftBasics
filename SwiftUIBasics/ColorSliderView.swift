ef957e8b7bfbeb2bed0b2e8df7514a39e810497e
uriarte <uriarte0505@gmail.com>
2021-04-09 17:11:35 -0300

    Commiting a simple iOS colour picker app

---
+++ SwiftUIBasics/ColorSliderView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorSliderView.swift
+//  SwiftUIBasics
+//
+//  Created by Victor Uriarte on 09/04/21.
+//
+
+import SwiftUI
+
+struct ColorSliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorSliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSliderView()
+    }
+}
---
+++ SwiftUIBasics/ColorSwatch.swift
@@ -0,0 +1,20 @@
+//
+//  ColorSwatch.swift
+//  SwiftUIBasics
+//
+//  Created by Victor Uriarte on 09/04/21.
+//
+
+import SwiftUI
+
+struct ColorSwatch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorSwatch_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSwatch()
+    }
+}
