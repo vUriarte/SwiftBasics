d8e90c3b8332edebb59869d4b594637faa9c6c7e
uriarte <uriarte0505@gmail.com>
2021-04-09 15:10:56 -0300

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Victor Uriarte on 09/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
